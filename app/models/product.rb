8f2a1aefa16a190a8fdde16ef7bf7db26a71911d
Derek Maffett <derekgmaffett@gmail.com>
2014-07-13 16:36:05 -0700

    add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+	validates :title, { uniqueness: true }
+
+	# allow_blank: true was claimed by the tutorial to be for preventing two errors on blank input.
+	# This seems imperfect to me as the code does not clearly represent the developer's intentions. 
+	validates :image_url, allow_blank: true, 
+		format: { with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG, or PNG image.'}
+
 end
